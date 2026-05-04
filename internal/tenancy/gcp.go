6da2b035f57e3378fd5a0b76c94a45515ca0f17b
Peter Vyboch <pvyboch1@gmail.com>
2026-05-04 16:59:25 +0200

    feat(tenancy): EMBYR_GCP_SECRETMANAGER_EMULATOR_HOST override for GCPSecretResolver

--- internal/tenancy/gcp.go
+++ internal/tenancy/gcp.go
@@ -3,9 +3,13 @@ package tenancy
 import (
 	"context"
 	"fmt"
+	"os"
 
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 // GCPSecretResolver fetches a DSN from GCP Secret Manager.
@@ -16,7 +20,16 @@ type GCPSecretResolver struct {
 }
 
 func (r *GCPSecretResolver) Resolve(ctx context.Context) (string, error) {
-	client, err := secretmanager.NewClient(ctx)
+	var opts []option.ClientOption
+	if host := os.Getenv("EMBYR_GCP_SECRETMANAGER_EMULATOR_HOST"); host != "" {
+		conn, err := grpc.NewClient(host, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			return "", fmt.Errorf("tenancy: gcp emulator dial %q: %w", host, err)
+		}
+		opts = append(opts, option.WithGRPCConn(conn))
+	}
+
+	client, err := secretmanager.NewClient(ctx, opts...)
 	if err != nil {
 		return "", fmt.Errorf("tenancy: gcp secret manager client: %w", err)
 	}
